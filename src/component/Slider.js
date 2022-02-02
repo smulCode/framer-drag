b47d00b68558d13bc3ca9f48581e86f5ce388b95
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2022-02-02 09:44:34 +0100

    v1 slider set up

---
+++ src/component/Slider.js
@@ -0,0 +1,10 @@
+import {motion} from 'framer-motion'
+import {useRef, useEffect, useState} from 'react'
+
+
+
+const Slider = () => {
+  return (<motion.h1 animate={{x:250}}>Slider</motion.h1>);
+};
+
+export default Slider;
